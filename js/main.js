ab0dfcbb51f30c31ffbcebd5455e3aee4e246839
Francine Bergamin <francine.bergamin@gmail.com>
2022-08-29 23:27:08 -0300

    Página criada: post equipe

--- js/main.js
+++ js/main.js
@@ -500,3 +500,19 @@ function showPassword() {
       x.type = "password";
     }
   }
+
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+  o.addEventListener("click", () => {
+    selected.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
+  });
+});
