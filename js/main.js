e2f3346f2e026fe46f412b7ff671cc06e9a13c3e
Gabriel <gomes.g.r@outlook.com>
2022-09-10 16:33:14 -0300

    Video input fixed
    added read video function into the html file

--- js/main.js
+++ js/main.js
@@ -905,24 +905,3 @@ optionsList.forEach(o => {
     });
 });
 
-/* video teste */
-const videoSrc = document.querySelector("#video-source");
-const videoTag = document.querySelector("#video-tag");
-const inputTag = document.querySelector("#input-tag");
-
-inputTag.addEventListener('change',  readVideo)
-
-function readVideo(event) {
-  console.log(event.target.files)
-  if (event.target.files && event.target.files[0]) {
-    var reader = new FileReader();
-    
-    reader.onload = function(e) {
-      console.log('loaded')
-      videoSrc.src = e.target.result
-      videoTag.load()
-    }.bind(this)
-
-    reader.readAsDataURL(event.target.files[0]);
-  }
-}
